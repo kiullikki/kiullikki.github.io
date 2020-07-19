a615b0eb975694ff9fec550731d05ca94514c32e
kiullikki <kiullikki@gmail.com>
2020-07-20 02:26:43 +0300

    Add files via upload
    added test project

---
+++ precache-manifest.f1cb2bee6ce6dbd1a814dc61a839f62e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c1945eafc11c3c29e9a0093769dbe8c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddacd4adf54231f4c47e",
+    "url": "/static/css/main.8f6add6b.chunk.css"
+  },
+  {
+    "revision": "18f3a7e8b24d6c31511e",
+    "url": "/static/js/2.947e42ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.947e42ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddacd4adf54231f4c47e",
+    "url": "/static/js/main.3fb028a2.chunk.js"
+  },
+  {
+    "revision": "f71d149245a00d868e35",
+    "url": "/static/js/runtime-main.d845c1db.js"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
